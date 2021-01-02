66cb5fa9a1b25aee09972184dbe938a0a920ae4c
Kyle Campbell <elyk.llebpmac@gmail.com>
2021-01-02 15:43:12 -0600

    cw query written

--- cryptoClient.py
+++ cryptoClient.py
@@ -1,41 +1,40 @@
 import cryptowatch as cw
+from datetime import datetime, timedelta
 
 # metrics to create could be stored in DB from user end
 # then createMetrics retrieves and forms the requests
-#def createMetricData():
-    # btc_data = {
-    #     'exchange': 'binance',
-    #     'pair': 'btcusdt',
-    #     'route': 'summary'
-    # }
-    # eth_data = {
-    #     'exchange': 'binance',
-    #     'pair': 'ethbtc',
-    #     'route': 'summary'
-    # }
-    # ltc_data = {
-    #     'exchange': 'binance',
-    #     'pair': 'ltcbtc',
-    #     'route': 'summary'
-    # }
-    # nano_data = {
-    #     'exchange': 'binance',
-    #     'pair': 'nanobtc',
-    #     'route': 'summary'
-    # }
-    #return
-    # to be replaced with markets from db 
-#     for market in kraken.markets:
-
-#         # Forge current market ticker, like KRAKEN:BTCUSD
-#         ticker = "{}:{}".format(market.exchange, market.pair).upper()
-#         # Request weekly candles for that market
-#         candles = cw.markets.get(ticker, ohlc=True, periods=["1w"])
-#     return [btc_data, eth_data, ltc_data, nano_data]
 
 
-# def query():
-#     metrics = createMetricData()
-#     for market in metrics:
-        
-#         print(response)
+# TODO: startup procedure of first 24 hours of data
+
+# TODO: create limiter to turn off once queries exceed limit
+
+def query():
+    exchange = 'binance'
+    pairs = ['btcusdt', 'ethbtc', 'ltcbtc', 'nanobtc']
+    for pair in pairs:
+        # Forge current market ticker, like KRAKEN:BTCUSD
+        ticker = "{}:{}".format(exchange, pair).upper()
+        print(ticker)
+        # Request weekly candles for that market
+        time = datetime.now() - timedelta(minutes=1)
+        min_ago = time.strftime("%s")
+        candle = cw.markets.get(ticker, ohlc=True, periods=['1m']) 
+        processCandle(candle)
+    return 
+
+
+def processCandle(candle):
+
+    # Each candle is a list of [close_timestamp, open, high, low, close, volume, volume_quote]
+    print('made it')
+    return
+
+
+def main():
+
+    data = query()
+    print(data)
+
+if __name__ == "__main__":
+    main()
